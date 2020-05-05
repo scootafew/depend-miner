765b18f921982ac2fc9714831436a48eefab19f3
Scott Pinwell <20837812+scootafew@users.noreply.github.com>
2020-05-05 18:37:34 +0100

    Removed old api-client

--- api-client/src/queues.ts
+++
@@ -1,5 +0,0 @@
-import * as Queue from 'bull';
-
-export const queues = {
-  "process": new Queue('process', process.env.REDIS_URL)
-};
--- api-client/src/worker.ts
+++
@@ -1,8 +0,0 @@
-import * as Worker from 'bull';
-import { Job } from 'bull';
-
-const worker = new Worker('process', process.env.REDIS_URL);
-
-worker.process(async (job: Job<any>) => {
-  console.log(`Processed job: ${job.data.job}`);
-});
